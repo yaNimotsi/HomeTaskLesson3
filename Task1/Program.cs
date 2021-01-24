fe9422c54b5e2859eed8892fe45288404e1ebb94
yaNimotsi <yanimotsi@yanmdex.ru>
2021-01-24 22:29:12 +0300

    Task1. Creater array, and set random val. Write to console main diagonal elements array

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,23 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter range array");
+            int range = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[range, range];
+
+            var rand = new Random();
+
+            for (int i = 0; i < range; i++)
+            {
+                for (int j = 0; j < range; j++)
+                {
+                    arr[i, j] = rand.Next(100);
+                }
+                Console.WriteLine(arr[i, i]);
+            }
+
+
         }
     }
 }
